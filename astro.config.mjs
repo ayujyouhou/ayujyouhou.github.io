46fff145f722bea9b63bd69a917381c6e7c1eef6
ayumu.nakajima.01 <you@example.com>
2025-09-02 18:35:41 +0900

    public

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ayujyouhou.github.io',   // ← GitHub Pages のURLを追加
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
